bbce9ed7d268389e10407d27aee24884ec52a3f4
engels74 <141435164+engels74@users.noreply.github.com>
2024-10-29 21:51:38 +0100

    revert docstrings

--- main.py
+++ main.py
@@ -245,3 +245,19 @@ if __name__ == "__main__":
 
 # TGraph - Tautulli Graph Bot
 # <https://github.com/engels74/tgraph-bot-source>
+# This script/bot works by posting Tautulli graphs to Discord webhook
+# Copyright (C) 2024 - engels74
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+# Contact: engels74@tuta.io
