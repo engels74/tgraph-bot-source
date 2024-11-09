260615969e16aeb912afb47b36e937d09807f6bc
engels74 <141435164+engels74@users.noreply.github.com>
2024-11-09 12:44:43 +0100

    codefix: bot\permission_checker.py

--- bot/permission_checker.py
+++ bot/permission_checker.py
@@ -94,13 +94,11 @@ async def resolve_permission_entities(
                     entities.append(f"Unknown Role {entity_id} ({permission_allowed})")
 
             elif perm["type"] == 2:  # User
-                try:
-                    user = await bot.fetch_user(entity_id)
-                    entities.append(f"{user.name} ({permission_allowed})")
-                except discord.NotFound:
-                    if show_unknown:
-                        logging.debug(f"User {entity_id} not found")
-                        entities.append(f"Unknown User {entity_id} ({permission_allowed})")
+                if member := guild.get_member(entity_id):
+                    entities.append(f"{member.name} ({permission_allowed})")
+                elif show_unknown:
+                    logging.debug(f"Member {entity_id} not found in guild {guild.name}")
+                    entities.append(f"Unknown Member {entity_id} ({permission_allowed})")
 
             elif perm["type"] == 3:  # Channel
                 if channel := guild.get_channel(entity_id):
